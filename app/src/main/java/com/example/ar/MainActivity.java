da08e01d5a9af7b7d935eba65ffd327fcbefc762
zongxinc <zongxinc@bu.edu>
2020-03-27 23:54:28 -0400

    yes

--- app/src/main/java/com/example/ar/MainActivity.java
+++ app/src/main/java/com/example/ar/MainActivity.java
@@ -3,10 +3,25 @@ package com.example.ar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.se.omapi.Session;
+import android.widget.Toast;
 
+import com.google.ar.core.AugmentedImage;
+import com.google.ar.core.AugmentedImageDatabase;
+import com.google.ar.core.Config;
+import com.google.ar.core.Frame;
+import com.google.ar.core.TrackingState;
+import com.google.ar.core.exceptions.CameraNotAvailableException;
+import com.google.ar.core.exceptions.UnavailableApkTooOldException;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableArcoreNotInstalledException;
+import com.google.ar.core.exceptions.UnavailableDeviceNotCompatibleException;
+import com.google.ar.core.exceptions.UnavailableSdkTooOldException;
 import com.google.ar.sceneform.ArSceneView;
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.Scene;
 import com.google.ar.sceneform.SceneView;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
@@ -15,7 +30,11 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+
+public class MainActivity extends AppCompatActivity implements Scene.OnUpdateListener {
 
     private ArSceneView arView;
     private Session session;
@@ -35,12 +54,15 @@ public class MainActivity extends AppCompatActivity {
                 .withListener(new PermissionListener() {
                     @Override
                     public void onPermissionGranted(PermissionGrantedResponse response) {
+                        setupSession();
 
                     }
 
+
+
                     @Override
                     public void onPermissionDenied(PermissionDeniedResponse response) {
-
+                        Toast.makeText(MainActivity.this, "Permission needed to display camera", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -49,5 +71,135 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }).check();
 
+        initSceneView();
+
+    }
+
+    private void initSceneView() {
+        arView.getScene().addOnUpdateListener(this);
+    }
+
+    private void setupSession() {
+        if(session == null) {
+            try {
+                session = new Session(this);
+            } catch (UnavailableArcoreNotInstalledException e) {
+                e.printStackTrace();
+            } catch (UnavailableApkTooOldException e) {
+                e.printStackTrace();
+            } catch (UnavailableSdkTooOldException e) {
+                e.printStackTrace();
+            } catch (UnavailableDeviceNotCompatibleException e) {
+                e.printStackTrace();
+            }
+            shouldConfigureSession = true;
+
+        }
+        if (shouldConfigureSession)
+        {
+            configureSession();
+            shouldConfigureSession = false;
+            arView.setupSession(session);
+        }
+
+        try {
+            session.resume();
+            arView.resume();
+        } catch (CameraNotAvailableException e) {
+            e.printStackTrace();
+            session= null;
+            return;
+        }
+    }
+
+    private void configureSession() {
+        Config config = new Config(session);
+        if(!builDatabase(config))
+        {
+            Toast.makeText(this, "Error databse", Toast.LENGTH_SHORT).show();
+        }
+        config.setUpdateMode(Config.UpdateMode.LATEST_CAMERA_IMAGE);
+        session.configure(config);
+    }
+
+    private boolean builDatabase(Config config) {
+        AugmentedImageDatabase augmentedImageDatabase;
+        Bitmap bitmap = loadImage();
+        if (bitmap == null)
+            return false;
+
+        augmentedImageDatabase = new AugmentedImageDatabase(session);
+        augmentedImageDatabase.addImage("temple", bitmap);
+        config.setAugmentedImageDatabase(augmentedImageDatabase);
+        return true;
+    }
+
+    private Bitmap loadImage() {
+        try {
+            InputStream is = getAssets().open("temple_qr.jpeg");
+            return BitmapFactory.decodeStream(is);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    @Override
+    public void onUpdate(FrameTime frameTime) {
+        Frame frame = arView.getArFrame();
+        Collection<AugmentedImage> updateAugmentedImg = frame.getUpdatedTrackables(AugmentedImage.class);
+
+        for (AugmentedImage image:updateAugmentedImg)
+        {
+            if(image.getTrackingState() == TrackingState.TRACKING)
+            {
+                if(image.getName().equals("temple"))
+                {
+                    MyARNode node = new MyARNode(this, R.raw.temple);
+                    node.setImage(image);
+                    arView.getScene().addChild(node);
+                }
+            }
+        }
+    }
+
+    //Ctrl + o
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        setupSession();
+
+                    }
+
+
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(MainActivity.this, "Permission needed to display camera", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(session != null)
+        {
+            arView.pause();
+            session.pause();
+        }
     }
 }
